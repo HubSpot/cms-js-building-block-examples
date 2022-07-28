0e1704c5c5e074bcc73fe9d7c66e757add924920
Jesse M <jmaltese@hubspot.com>
2022-07-28 11:34:05 -0400

    Initial commit

---
+++ js-rendering/components/partials/About.jsx
@@ -0,0 +1,12 @@
+// http://883342099.hs-sitesqa.com.localhost:3000/partial/About
+
+function About() {
+  return (
+    <>
+      <div>All About Me...</div>
+      <a href="./Home.jsx">Home.</a>
+    </>
+  )
+}
+
+export default About;
---
+++ js-rendering/components/partials/Home.jsx
@@ -0,0 +1,13 @@
+// http://883342099.hs-sitesqa.com.localhost:3000/partial/Home
+
+function Home() {
+  return (
+    <>
+      <div>Hello World!</div>
+      <a href="./About.jsx">About.</a>
+    </>
+
+  )
+}
+
+export default Home;
