cd1a4fb03e8bddf2a0522fd8e3e2209a66ef703f
Jesse M <jmaltese@hubspot.com>
2022-08-09 11:56:16 -0400

    Update deps

--- js-rendering/components/islands/Button.jsx
+++ js-rendering/components/islands/Button.jsx
@@ -4,15 +4,11 @@ import React, { useState } from 'react';
 function Button(props) {
   const [count, setCount] = useState(0)
   return (
-    <>
+    <span>
       <button onClick={() => setCount(count + 1)}>You've clicked me {count} times...</button>
-    </>
+    </span>
   )
-
 }
-
-
-
 
 
 export default Button;
--- js-rendering/components/partials/About.jsx
+++ js-rendering/components/partials/About.jsx
@@ -1,19 +1,18 @@
 // http://883342099.hs-sitesqa.com.localhost:3000/partial/About
 import { IslandRoot } from '@hubspot/cms-components';
-import Button from '../islands/Button?island';
+import Button from '../islands/Button.jsx?island';
 
 function About() {
   return (
     <>
       <div>All About Me...</div>
-      <IslandRoot
-        lazyModule={Button}
-        hydateOn="load"
-      ></IslandRoot>
-      <IslandRoot
-        lazyModule={Button}
-        hydateOn="load"
-      ></IslandRoot>
+      <div className='buttons'>
+        <Button/>
+        <IslandRoot
+          lazyModule={Button}
+          hydateOn="load"
+        ></IslandRoot>
+      </div>
       <a href="./Home.jsx">Home.</a>
     </>
   )
