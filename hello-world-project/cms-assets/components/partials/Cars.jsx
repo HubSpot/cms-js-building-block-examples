ec381a08cd779abaa2b6495250f39ff43a646292
Byron Matto <bmatto@hubspot.com>
2022-10-19 09:56:55 -0400

    Hard code cars for empathy

--- hello-world-project/cms-assets/components/partials/Cars.jsx
+++ hello-world-project/cms-assets/components/partials/Cars.jsx
@@ -2,27 +2,70 @@ import Layout from '../Layout';
 
 import styles from '../../styles/cars.module.css';
 
-function Cars({ carCollection }) {
-  const cars = JSON.parse(carCollection);
+import tacoma from '../../assets/tacoma.jpg';
+import vwid from '../../assets/vw-id.jpg';
+import orangeLine from '../../assets/orange-line.jpg';
+import huayra from '../../assets/huayra.jpg';
 
+const cars = [
+  {
+    year: 2009,
+    make: 'Toyota',
+    model: 'Tacoma',
+    id: 1,
+    img: tacoma,
+    contact: {
+      firstname: 'Byron',
+      lastname: 'Matto',
+    },
+  },
+  {
+    year: 1979,
+    make: 'MBTA',
+    model: 'Organe Line',
+    id: 2,
+    img: orangeLine,
+    contact: {
+      firstname: 'Ben',
+      lastname: 'Briggs',
+    },
+  },
+  {
+    year: 2021,
+    make: 'Volkswagen',
+    model: 'ID',
+    id: 3,
+    img: vwid,
+    contact: {
+      firstname: 'Timothy',
+      lastname: 'Finley',
+    },
+  },
+  {
+    year: 2019,
+    make: 'Pagani',
+    model: 'Huayra',
+    id: 4,
+    img: huayra,
+    contact: {
+      firstname: 'Charles',
+      lastname: 'Boamah',
+    },
+  },
+];
+
+function Cars() {
   return (
     <Layout>
       <h2>My Teamates Cars</h2>
       <div className={styles.cars}>
         {cars.map((car) => {
           return (
-            <div key={car.hs_object_id} className={styles.carGrid}>
+            <div key={car.id} className={styles.carGrid}>
               <div className={styles.car}>
                 <div className={styles.carInfo}>
                   <h2>
-                    {
-                      car.associations.contact_collection__car_to_contact
-                        .items[0].firstname
-                    }{' '}
-                    {
-                      car.associations.contact_collection__car_to_contact
-                        .items[0].lastname
-                    }
+                    {car.contact.firstname} {car.contact.lastname}
                   </h2>
                   <p>
                     <span>
@@ -30,10 +73,9 @@ function Cars({ carCollection }) {
                     </span>
                   </p>{' '}
                 </div>
-                <div
-                  className={styles.carDetails}
-                  dangerouslySetInnerHTML={{ __html: car.details }}
-                />
+                <div className={styles.carDetails}>
+                  <img src={car.img} />
+                </div>
               </div>
             </div>
           );
