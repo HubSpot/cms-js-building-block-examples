4fba3b311b1072e3dccac738ecfb3ad310700e10
Byron Matto <bmatto@hubspot.com>
2022-10-25 16:15:00 -0400

    Pass theme settings

--- hello-world-project/cms-assets/components/partials/Header.jsx
+++ hello-world-project/cms-assets/components/partials/Header.jsx
@@ -1,20 +1,46 @@
 import headerStyles from '../../styles/header.module.css';
 import { usePageUrl } from '@hubspot/cms-components';
 
-export default function Header() {
+export default function Header(props) {
   const { origin } = usePageUrl();
   const pageUrlHome = `${origin}/hello-world-home`;
   const pageUrlTodo = `${origin}/hello-world-todo`;
   const pageUrlCars = `${origin}/hello-world-cars`;
+
+  const brandColor = JSON.parse(props.brandColor);
+
+  const navLinks = [
+    {
+      href: pageUrlHome,
+      label: 'Home',
+    },
+    {
+      href: pageUrlTodo,
+      label: 'Todo',
+    },
+    {
+      href: pageUrlCars,
+      label: 'Cars',
+    },
+  ];
 
   return (
     <header className={headerStyles.header}>
       <h1>Hello World</h1>
       <p>The World Sends Greetings!</p>
       <nav className={headerStyles.nav}>
-        <a href={pageUrlHome}>Home</a>
-        <a href={pageUrlTodo}>ToDo</a>
-        <a href={pageUrlCars}>Cars</a>
+        {navLinks.map((navLink) => (
+          <a
+            style={{
+              color: brandColor.color,
+              borderColor: brandColor.color,
+            }}
+            key={navLink.label}
+            href={navLink.href}
+          >
+            {navLink.label}
+          </a>
+        ))}
       </nav>
     </header>
   );
