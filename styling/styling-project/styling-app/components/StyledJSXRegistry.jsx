aa7c600d7dba9321ce37ed62f9413053e71c7105
Ben Briggs <bbriggs@hubspot.com>
2023-07-11 15:48:19 -0400

    add more comments for styled-jsx

--- styling/styling-project/styling-app/components/StyledJSXRegistry.jsx
+++ styling/styling-project/styling-app/components/StyledJSXRegistry.jsx
@@ -4,12 +4,19 @@ import { useServerInsertedHtml } from '@hubspot/cms-components';
 function InsertHTML() {
   const registry = useStyleRegistry();
   useServerInsertedHtml(() => {
+    // Collect styles generated on the server pass and return them to go in the
+    // <head>
+
     const styles = registry.styles();
     return <>{styles}</>;
   });
 }
 
 export default function StyledJSXRegistry({ children }) {
+  // for styled-jsx, the registry component needs to be included on the client
+  // and server to make sure that hook usage is preserved. excluding it on the
+  // client can result in mismatching results of useId()
+
   return (
     <StyleRegistry>
       <InsertHTML />
