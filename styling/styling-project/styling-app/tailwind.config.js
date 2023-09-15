432fe8eb50033492f42c8e0089d63eebff79c263
Timothy Finley <tfinley@hubspot.com>
2023-09-15 11:12:56 -0400

    Fix windows path for tailwind

--- styling/styling-project/styling-app/tailwind.config.js
+++ styling/styling-project/styling-app/tailwind.config.js
@@ -1,4 +1,5 @@
-const componentsDir = new URL('./components', import.meta.url).pathname;
+import { fileURLToPath } from 'url';
+const componentsDir = fileURLToPath(new URL('./components', import.meta.url));
 
 export default {
   content: [`${componentsDir}/**/*.{js,ts,jsx,tsx}`],
