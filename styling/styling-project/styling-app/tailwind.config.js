19b511b78b213b7ec1c71deb34e8a17dc0179078
Timothy Finley <tfinley@hubspot.com>
2023-09-15 11:15:35 -0400

    Simplify with relative path

--- styling/styling-project/styling-app/tailwind.config.js
+++ styling/styling-project/styling-app/tailwind.config.js
@@ -1,8 +1,6 @@
-import { fileURLToPath } from 'url';
-const componentsDir = fileURLToPath(new URL('./components', import.meta.url));
-
 export default {
-  content: [`${componentsDir}/**/*.{js,ts,jsx,tsx}`],
+  relative: true,
+  content: [`./components/**/*.{js,ts,jsx,tsx}`],
   theme: {
     extend: {},
   },
